10f4863f8ac450ae28385d50d899b147bef6cafa
Álvaro Henrique <alvaroh.edf@gmail.com>
2026-08-22 20:51:40 -0300

    Add files via upload

---
+++ lista 02.py
@@ -0,0 +1,37 @@
+'''
+QUESTAO 01
+
+numero = 0
+while numero <= 100:
+    print(numero)
+    numero += 1
+'''
+#---------------------------------------------------------------------------------------
+'''
+QUESTÃO 02
+
+numero = 0
+n = int(input("Insira o limite: "))
+
+while numero <= n:
+    print(numero)
+    numero += 1
+'''
+#--------------------------------------------------------------------------------------
+'''
+QUESTÃO 03
+print("OPERAÇÃO - ADIÇÃO")
+
+rep = 'S'
+while rep == 'S':
+
+    a = int(input("Digite um número: "))
+    b = int(input("Digite outro número: "))
+    n = a+b
+
+    print(f"A soma: {a} + {b} = {n}")
+
+    rep = input("Deseja realizar mais uma soma? (S ou N)\n Resposta: ").upper()
+
+print("Programa encerrado.")
+'''
